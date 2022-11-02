22041386116eb17dbabe1178f10556049c5e63d7
alanmaydwell <alan.maydwell@digital.justice.gov.uk>
2022-11-02 14:22:13 +0000

    pricing of items

--- supermarket.py
+++ supermarket.py
@@ -1,7 +1,12 @@
+import pricing
 
 products = {"FR1": "Fruit tea",
             "SR1": "Strawberries",
             "CF1": "Coffee"}
+
+product_price_params = {"FR1": [3.11, pricing.bogof],
+                        "SR1": [5.00, pricing.special_sr1],
+                        "CF1": [11.23, pricing.simple]}
 
 
 class Checkout:
@@ -12,15 +17,24 @@ class Checkout:
         self.items = {k:0 for k in products}
 
     def add_item(self, item_code, quantity=1):
-        self.items[item_code] +=quantity
+        self.items[item_code] += quantity
         
     def get_prices(self):
-        pass
+        total = 0.0
+        for product_code, quantity in self.items.items():
+            unit_price, price_function = product_price_params[product_code]
+            item_price = price_function(quantity, unit_price)
+            print(f"Code:{product_code} Quantity:{quantity} price:{item_price}")
+            total += item_price
+        print("Total:", total)
+            
     
-        
 if __name__ == "__main__":
     basket = Checkout()
     basket.add_item("FR1")
     basket.add_item("SR1")
     basket.add_item("CF1")
-    print(basket.items)
+    basket.get_prices()
+    basket.add_item("FR1", 3)
+    basket.get_prices()
+    
