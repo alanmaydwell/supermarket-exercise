a2ba81f3654f9f74d1910ba281b57058e2593bfa
alanmaydwell <alan.maydwell@digital.justice.gov.uk>
2022-11-03 08:47:04 +0000

    improvements to price calc, automated checks, jupyter notebook

--- supermarket.py
+++ supermarket.py
@@ -12,29 +12,25 @@ product_price_params = {"FR1": [3.11, pricing.bogof],
 class Checkout:
     def __init__(self):
         self.reset_basket()
-        
+
     def reset_basket(self):
         self.items = {k:0 for k in products}
 
     def add_item(self, item_code, quantity=1):
         self.items[item_code] += quantity
-        
+
     def get_prices(self):
-        total = 0.0
+        calculated_totals = {"Total": 0}
         for product_code, quantity in self.items.items():
             unit_price, price_function = product_price_params[product_code]
             item_price = price_function(quantity, unit_price)
-            print(f"Code:{product_code} Quantity:{quantity} price:{item_price}")
-            total += item_price
-        print("Total:", total)
-            
-    
-if __name__ == "__main__":
-    basket = Checkout()
-    basket.add_item("FR1")
-    basket.add_item("SR1")
-    basket.add_item("CF1")
-    basket.get_prices()
-    basket.add_item("FR1", 3)
-    basket.get_prices()
-    
+            calculated_totals[product_code] = item_price
+            calculated_totals["Total"] += item_price
+        return calculated_totals
+
+    def show_prices(self):
+        price_details = self.get_prices()
+        for name, cost in price_details.items():
+            if name in products:
+                name = products[name]
+            print(f"{name}: {cost:.2f}")
